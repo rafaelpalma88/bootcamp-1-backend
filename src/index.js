e8b5d40a07500b86cc8d161287a8159fce8f7b09
Rafael Costa Palma <palma3482@gmail.com>
2020-04-09 18:56:08 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const { uuid, isUuid } = require('uuidv4')
+
+const app = express();
+
+app.use(express.json())
+
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no backend
+ * PUT/PATCH: Alterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ *  
+ * Tipos de parametros:
+ * 
+ * Query Params: Filtros, paginação 
+ * Route Params: Identificar recursos na hora de atualizar ou deletar
+ * Request Body: Conteudo na hora de criar ou editar um recurso ( JSON )
+ * 
+ * Middleware: Interceptador de requisições que interromper totalmente a requisição ou alterar dados da requisição
+ * 
+ */
+
+const projects = [];
+
+function logRequests(request, response, next) {
+    const { method, url } = request;
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`
+
+    console.log(logLabel)
+
+    return next()
+}
+
+function validateProjectId(request, response, next) {
+   
+    const { id } = request.params
+
+    if(!isUuid(id)) {
+        return response.status(400).json({ error: 'Invalid project ID'})
+    }
+
+    return next();
+}
+
+app.use(logRequests)
+
+app.get('/projects', (request, response) => {
+
+    const { title } = request.query;
+
+    const results = title
+        ? projects.filter(project => project.title.includes(title))
+        : projects
+
+    return response.json(results)
+})
+
+app.post('/projects', (request, response) => {
+
+    const { title, owner } = request.body;
+    
+    const project = { id: uuid(), title, owner }
+
+    projects.push(project)
+
+    return response.json(project)
+})
+
+app.put('/projects/:id', validateProjectId, (request, response) => {
+
+    const { id } = request.params
+    const { title, owner } = request.body;
+
+    const projectIndex = projects.findIndex(project => project.id === id)
+
+    if (projectIndex < 0) {
+        response.status(400).json({ error: 'Project not found' })
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+
+    projects[projectIndex] = project
+
+    return response.json(project)
+})
+
+app.delete('/projects/:id', (request, response) => {
+
+    const { id } = request.params
+
+    const projectIndex = projects.findIndex(project => project.id === id)
+
+    if (projectIndex < 0) {
+        response.status(400).json({ error: 'Project not found' })
+    }
+
+    projects.splice(projectIndex, 1)
+
+    return response.status(204).send()
+})
+
+app.listen(3333, () => {
+    console.log('backend started')
+})
